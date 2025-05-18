de19aaaff569b5919bd6017b1cab865e010548f2
VadrasCZ <vmzourek@gmail.com>
2025-05-18 13:43:58 +0200

    I added 4 trees and i changed the function.

--- pygame_game.py
+++ pygame_game.py
@@ -2,34 +2,36 @@ import turtle as t
 
 t.colormode(255)  # Allow RGB values from 0 to 255
 
-# Funkce vykresli parez
-def create_parez():
-    t.fillcolor((139, 69, 19))  # A brownish color for the "parez" (stump)
+#Function that creates a tree
+def create_strom(x, y):
+    t.penup()
+    t.fillcolor((139, 69, 19))
+    t.goto(x, y)
+    t.pendown()
     t.begin_fill()
     t.setheading(90)
-    for _ in range(4):
+    for i in range(4):
         t.forward(90)
         t.right(90)
-    t.end_fill()  # Called after the shape is completed
+    t.end_fill()
 
-# Funkce vykresli listy
-def create_listy():
-    t.fillcolor((69, 140, 3))
+    t.fillcolor((27, 110, 7))
     t.begin_fill()
-    t.penup()
-    t.goto(0, 90)
-    t.pendown()
-    t.left(90)
-    t.forward(45)
-    t.right(135)
-    t.goto(45, 300)
-    t.goto(135, 90)
-    t.goto(0, 90)
+    t.goto(x, y + 90)
+    t.goto(x - 45, y + 90)
+    t.goto(x + 45, y + 325)    
+    t.goto(x + 135, y + 90)
+    t.goto(x + 90, y + 90)
+    t.goto(x, y + 90)
     t.end_fill()
 
     
-# Create the drawing
-create_parez()
-create_listy()
+
+# Creating 5 trees 180 pixels between themselves
+create_strom(-360, 0)
+create_strom(-180, 0)
+create_strom(0, 0)
+create_strom(180, 0)
+create_strom(360, 0)
 
 t.mainloop()
